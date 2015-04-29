ca552d90c4188c0d2245fdfbede0d5c9dd76c64c
agconti <agc11d@gmail.com>
2015-04-29 11:24:39 -0400

    feat(test/controllers): completed basic unit tests

--- test/controllers.js
+++ test/controllers.js
@@ -4,11 +4,14 @@ var should = require('chai').should()
   , auth = require('../')
   , fixtures = require('./fixtures')
   , UnauthorizedError = require('../lib/errors/UnauthorizedError')
+  , request = require('supertest')
+  , express = require('express')
+  , next = function(){}
   , req 
   , res
 
 describe('ensureAuthorizationHeader', function(){
-  it('shoud add the split authorization header to req.auth', function(done){
+  it('should add the split authorization header to req.auth', function(done){
     req = httpMocks.createRequest()
     res = httpMocks.createResponse()
     req.headers.authorization = fixtures.authorization
@@ -30,75 +33,79 @@ describe('ensureAuthorizationHeader', function(){
     })
     it('request headers are not defined', function(done){
       req.headers = undefined
-      auth.ensureAuthorizationHeader(req, res, function(){})
+      auth.ensureAuthorizationHeader(req, res, next)
       res.statusCode.should.be.equal(401)
       res._getData().name.should.be.equal('UnauthorizedError')
       done()
     })
     it('authorization headers are not defined', function(done){
-      auth.ensureAuthorizationHeader(req, res, function(){})
+      auth.ensureAuthorizationHeader(req, res, next)
       res.statusCode.should.be.equal(401)
       res._getData().name.should.be.equal('UnauthorizedError')
       done()
     })
     it('the authorization header is not name spaced', function(done){
       req.headers.authorization = fixtures.NotNameSpacedAuth
-      auth.ensureAuthorizationHeader(req, res, function(){})
+      auth.ensureAuthorizationHeader(req, res, next)
       res.statusCode.should.be.equal(401)
       res._getData().name.should.be.equal('UnauthorizedError')
       done()
     })
     it('the credentials string contains spaces', function(done){
       req.headers.authorization = fixtures.jwtAuthWithSpaces
-      auth.ensureAuthorizationHeader(req, res, function(){})
+      auth.ensureAuthorizationHeader(req, res, next)
       res.statusCode.should.be.equal(401)
       res._getData().name.should.be.equal('UnauthorizedError')
       done()
-
     })      
   })
 })
 describe('validateJWTAuth', function(){
   beforeEach(function(){
-      req = httpMocks.createRequest()
-      res = httpMocks.createResponse()
+    req = httpMocks.createRequest()
+    res = httpMocks.createResponse()
   })
   it('should exit early if authorization is not JWT', function(done){
-      req.auth = fixtures.OAUTH.split(' ')
-      auth.validateJWTAuth(req, res, done)
-    })
+    req.auth = fixtures.OAUTH.split(' ')
+    auth.validateJWTAuth(req, res, done)
+  })
   it('should fail with an invalid JWT token', function(done){
-      req.auth = fixtures.invalidJWTAuth.split(' ')
-      auth.validateJWTAuth(req, res, function(){})
+    req.auth = fixtures.invalidJWTAuth.split(' ')
+    setTimeout(function(){
       res.statusCode.should.be.equal(401)
-      res._getData().name.should.be.equal('UnauthorizedError')
+      var data = res._getData()
+      data.name.should.be.equal('JsonWebTokenError')
+      data.message.should.be.equal('invalid token')
       done()
-    })
+    }, 100)
+    auth.validateJWTAuth(req, res, next)
+  })
   it('should assign the decoded JWT token to req.user', function(done){
-      req.auth = fixtures.authorization.split(' ')
-      auth.validateJWTAuth(req, res, function(){})
+    req.auth = fixtures.authorization.split(' ')
+    auth.validateJWTAuth(req, res, next)
+    setTimeout(function(){
       should.exist(req.user)
       req.user.id.should.equal(fixtures.user.id)
       req.user.name.should.equal(fixtures.user.name)
       done()
-    })
+    }, 200)
+  })
 })
 describe('ensureAuthorized', function(){
   beforeEach(function(){
-      req = httpMocks.createRequest()
-      res = httpMocks.createResponse()
+    req = httpMocks.createRequest()
+    res = httpMocks.createResponse()
   })
   it('should fail when req.user is not defined', function(done){
-      auth.ensureAuthorized(req, res, function(){})
-      res.statusCode.should.be.equal(401)
-      res._getData().name.should.be.equal('UnauthorizedError')
-      done()
-    })
-  it('should succed when req.user is defined', function(done){
-      req.user = true
-      auth.ensureAuthorized(req, res, done)
-      done()
-    })
+    auth.ensureAuthorized(req, res, next)
+    res.statusCode.should.be.equal(401)
+    res._getData().name.should.be.equal('UnauthorizedError')
+    done()
+  })
+  it('should succeed when req.user is defined', function(done){
+    req.user = true
+    auth.ensureAuthorized(req, res, done)
+  })
 })
 
  
